5b091fb1bc2a8cbc72f6e68b89506a3e5668fdaf
DOS <dsaban@amadeus.com>
2021-03-14 14:03:54 +0100

    Aggregation des cartes verticalement, correction de typos

--- generateImages.sh
+++ generateImages.sh
@@ -25,6 +25,6 @@ rm -rf _cards* _terrain*
 ruby src/main/ruby/carte.rb
 # ruby src/main/ruby/horde_cards.rb
 cd ../printableCardsAppender
-./gradlew appendCard --args="../ficanasse-cardgame/_cards ../ficanasse-cardgame/imagesToPrint/carte   A4 true"
+./gradlew appendCard --args="../ficanasse-cardgame/_cards ../ficanasse-cardgame/imagesToPrint/carte   A4 false"
 # ./gradlew appendCard --args="../windwalkers-cardgame/_cards1  ../windwalkers-cardgame/imagesToPrint/cards_v1_ A4 false"
 # ./gradlew appendCard --args="../windwalkers-cardgame/_cards_cut  ../windwalkers-cardgame/imagesToPrint/cards_cut_ A4 false"
